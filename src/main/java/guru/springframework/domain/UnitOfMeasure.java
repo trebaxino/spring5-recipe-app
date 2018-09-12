f984fbd94f6c01c36bc1bb576885b01e984b0c0f
Marfiak Tomas <tomas.marfiak@capco.com>
2018-09-12 15:28:05 +0200

    - Added domain entities

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,29 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String uom;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+}
